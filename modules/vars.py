3c8873d8a8359310f9f99607219f8bb5f6931345
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2024-04-09 22:41:14 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "12606917"
 api_hash = "f25113b8c17dca6fa7abda53a86bd4f7"
-bot_token = "6787006839:AAH6fb5KJvXCTW-odGE0Njfi7wfPvuzljj4"
+bot_token = "6787006839:AAHtJYpGKS71VpIfWl6-UGZCvskFzjOhhkk"
